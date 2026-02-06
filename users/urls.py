83719349a2c0d40318c69828d0b056801f28b298
Sanjo-Salisu <sanjosalisuskillz@gmail.com>
2026-02-06 16:35:31 +0100

    new

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register_customer, name='register_customer'),
+    path('', views.register_customer, name='register_customer'),
     path('scan/', views.scan_qr, name='scan_qr'),
 ]
 
